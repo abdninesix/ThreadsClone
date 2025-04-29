17b40ea30b61047736dc18d2e386dd62b6de3e5f
abdninesix <mohabd1995@gmail.com>
2025-04-29 18:40:25 +0500

    Updated

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className='h-screen md:h-[calc(100vh-80px)] flex flex-col-reverse md:flex-row gap-20 items-center justify-center duration-200'>
+      <div className='bg-white p-10 rounded-xl shadow-xl flex flex-col gap-4'>
+        <p className='font-bold'>Login with the following details for testing</p>
+        <p><b>Email: </b>testuser@test.com</p>
+        <p><b>Username: </b>testuser</p>
+        <p><b>Password: </b>testuser_12345</p>
+      </div>
+      <SignUp />
+    </div>
+  )
 }
