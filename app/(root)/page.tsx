0de1213d665a0058ba13ed673032f8a5d643b9ed
abdninesix <mohabd1995@gmail.com>
2025-05-02 08:43:45 +0500

    Updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
    <h1 className="head-text text-left">Home</h1>
    <section className="mt-9 flex flex-col gap-10">
       {result.posts.length === 0 ? (
-        <p className="no-result">No posts to display</p>
+        <p className="no-result">No posts here</p>
       ) : (
         <>
           {result.posts.map((post) => (
